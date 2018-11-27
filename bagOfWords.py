2630b72a82282af775cf3fa75bd1bc0a8a1141e4
Assaf Avital <assaf.avital9@gmail.com>
2018-11-27 14:20:58 +0200

    export google queries to text file

--- bagOfWords.py
+++ bagOfWords.py
@@ -5,19 +5,27 @@ sentences = ['Hello, how are you!',
              'Call me now.',
              'Hello, Call hello you tomorrow?']
 
+with open("stt_output.txt", "r") as f:
+    sentences = [f.read()]
+
 count_vector = CountVectorizer()
 
 # configuring sklearn learn CountVectorizer to set all words to lower case,
 # filter stop words and return max feature names
 count_vector.lowercase = True
 count_vector.stop_words = 'english'
-count_vector.max_features = 10
+count_vector.max_features = 16
 
 # print(count_vector)
 
 count_vector.fit(sentences)
-print(count_vector.get_feature_names())
+# print(count_vector.get_feature_names())
 
+query = ""
+for feature in count_vector.get_feature_names():
+    query += " {}".format(feature)
 
+with open("queries.txt", "w") as f:
+    f.write(query)
 
 
